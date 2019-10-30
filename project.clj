06ad316d610e57fea05a8951ccf3f0de7be13f2a
sandmark <sandmark.m@gmail.com>
2019-10-30 22:53:53 +0900

    Add dependency for reagent

--- project.clj
+++ project.clj
@@ -4,7 +4,8 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url  "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.1"]
-                 [org.clojure/clojurescript "1.10.520"]]
+                 [org.clojure/clojurescript "1.10.520"]
+                 [reagent "0.8.1"]]
   :repl-options {:init-ns react-tutor-cljs.core}
 
   :resource-paths ["resources" "target"]
